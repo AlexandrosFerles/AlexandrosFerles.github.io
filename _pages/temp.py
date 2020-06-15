d93e4de617eb3d8cae9fdb642d0a4af842d29a50
ferles <ferles@kth.se>
2020-06-15 08:59:03 +0200

    Updates

---
+++ _pages/temp.py
@@ -0,0 +1,14 @@
+---
+title: "\[Re\] Zero-Shot Knowledge Transfer via Adversarial Belief Matching"
+collection: publications
+permalink: /publication/2020-05-21-rescience
+excerpt: ''
+date: 2020-05-21
+venue: 'RESCIENCE C'
+paperurl: 'http://rescience.github.io/bibliography/Ferles_2020.html'
+citation: 'Ferles, A., Nöu, A., and Valavanis, L. (2020). &quot;\[Re\] Zero-Shot Knowledge Transfer via Adversarial Belief Matching.&quot; <i>RESCIENCE C</i>. 6(2).'
+---
+
+[Download paper here](https://zenodo.org/record/3818623/files/article.pdf)
+
+Ferles, A., Nöu, A., and Valavanis, L. 2020. \[Re\] Zero-Shot Knowledge Transfer via Adversarial Belief Matching. ReScience C 6, 2, #2.
